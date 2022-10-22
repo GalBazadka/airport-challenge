4512c7590d709fc19b61d9c65de2b35f4610d73d
Gal Bazadka <galbazadka@gmail.com>
2022-10-22 16:29:40 +0300

    changes

--- index.js
+++ index.js
@@ -1,40 +1,27 @@
-import { uniqueDestination,createFlight,counter} from "./modules/flightsManager.js";
 import all_data from './flights.json' assert { type:"json" }
 import log from '@ajar/marker'; 
+import Flight from './modules/flight.js';
+import FlightManager from './modules/flightsManager.js';
 
-const {flights} = all_data
+const flight_array = all_data.flights;
 
-// myFlight.on('FLIGHT ARRIVED', instance => {
-//     console.log(instance.origin)
-// })
+const fm = new FlightManager();
 
-// const arr = uniqueDestination(flights)
-// console.log(arr.length)
+for (let flight_data of flight_array) {
 
+    const flight = fm.create_flight(flight_data);
 
-const destinationArr = uniqueDestination(flights)
-const stringOfDestination = `${destinationArr}`
+    flight.on(Flight.FLIGHT_ARRIVED, print_flight_info);
 
-// console.log(arr)
+    flight.depart();
+}
 
-flights.forEach((flight) => {
-
-	// destructure the following data:
-	const { number, origin, destination } = flight;
-
-	// create a Flight instance using a FlightManager method
-	createFlight(number, origin, destination);
-	
-});
-setTimeout(() => {
-	log.magenta('===================================================')
-	log.yellow(destinationArr.length,' flights were created')
-	log.blue('destination: ',stringOfDestination)
-	log.magenta('===================================================')
-	
-}, 5000);
+log.magenta('=============================================');
+log.yellow(fm.flights_count , ' flights were created');
+log.d('destinations: ', fm.destinations);
+log.magenta('=============================================')
 
 
-// const allFlights = pushUniqueDestination(flights)
-
-// console.log(allFlights)
+function print_flight_info(instance) {
+    log.blue(`✈️ Arrived: ${instance.flight_number}`, ' from: ', instance.origin, ' to ', instance.destination, ' at ', instance.arrived);
+}
